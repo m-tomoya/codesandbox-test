c456ebcc92994465ba047ddadc506a17f40e1127
m-tomoya <salvatoleyohane@gmail.com>
2022-04-10 18:11:40 +0900

    javascript todo app complete

--- src/index.js
+++ src/index.js
@@ -4,19 +4,53 @@ const onClickAdd = () => {
   // テキストボックスの値を取得し、からにする
   const inputText = document.getElementById("input-text").value;
   document.getElementById("input-text").value = "";
+  createIncompleteList(inputText);
+};
 
+const deleteFromIncompletedList = (target) => {
+  document.getElementById("incompleted-list").removeChild(target);
+};
+
+const createIncompleteList = (text) => {
   const div = document.createElement("div");
   div.className = "list-row";
   console.log(div);
 
   const li = document.createElement("li");
-  li.innerText = inputText;
+  li.innerText = text;
 
   // COMPLETE BUTTON タグ生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "COMPLETE";
   completeButton.addEventListener("click", () => {
-    alert("COMPLETE");
+    deleteFromIncompletedList(completeButton.parentNode);
+    // 完了リストに追加する要素
+    const addTarget = completeButton.parentNode;
+    // TODO内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
+
+    // div以下を初期化する
+    addTarget.textContent = null;
+    console.log(addTarget);
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    const backButton = document.createElement("button");
+    backButton.innerText = "Back";
+
+    backButton.addEventListener("click", () => {
+      const deleteTarget = backButton.parentNode;
+      const text = deleteTarget.firstElementChild.innerText;
+      createIncompleteList(text);
+      document.getElementById("completed-list").removeChild(deleteTarget);
+    });
+
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    document.getElementById("completed-list").appendChild(addTarget);
+
+    console.log(li);
   });
 
   // DELETE BUTTON タグ生成
@@ -24,12 +58,8 @@ const onClickAdd = () => {
   deleteButton.innerText = "DELETE";
   deleteButton.addEventListener("click", () => {
     //押された削除ボタンの親タグを未完了リストから削除
-    const deleteTarget = deleteButton.parentNode;
-    console.log(deleteTarget);
-    document.getElementById("incompleted-list").removeChild(deleteTarget);
+    deleteFromIncompletedList(deleteButton.parentNode);
   });
-
-  console.log(li);
 
   div.appendChild(li);
   div.appendChild(completeButton);
