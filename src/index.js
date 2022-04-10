0618ac4c1ac9cea14313e534e6f99e9ecfd41b43
m-tomoya <salvatoleyohane@gmail.com>
2022-04-10 16:57:41 +0900

    DELETE BUTTON 実装

--- src/index.js
+++ src/index.js
@@ -1,93 +1,44 @@
-// // // /**
-// // //  * const let
-// // //  */
+import "./styles.css";
 
-// // // //  var val1 = "var変数"
-// // // //  console.log(val1)
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、からにする
+  const inputText = document.getElementById("input-text").value;
+  document.getElementById("input-text").value = "";
 
-// // // //  val1 = "上書き"
-// // // //  console.log(val1)
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
 
-// // // //  var val1 = "var変数を再宣言"
-// // // //  console.log(val1)
+  const li = document.createElement("li");
+  li.innerText = inputText;
 
-// // // let val2 = "let";
-// // // // let val2 = "console"
+  // COMPLETE BUTTON タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "COMPLETE";
+  completeButton.addEventListener("click", () => {
+    alert("COMPLETE");
+  });
 
-// // // const test = { type: "type", class: "class" };
-// // // test.type = "test";
-// // // console.log(test);
+  // DELETE BUTTON タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "DELETE";
+  deleteButton.addEventListener("click", () => {
+    //押された削除ボタンの親タグを未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    console.log(deleteTarget);
+    document.getElementById("incompleted-list").removeChild(deleteTarget);
+  });
 
-// // // // const name = "tomo";
-// // // const message1 = "mmy name" + name + ".";
-// // // console.log(message1);
+  console.log(li);
 
-// // // const message2 = `my name is ${name}.`
-// // // console.log(message2)
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  console.log(div);
 
-// // // const func1 = function(str){
-// // //   return str
-// // // }
+  document.getElementById("incompleted-list").appendChild(div);
+};
 
-// // // console.log(func1("string"))
-
-// // // const func2 = (str)=>{
-// // //   return str;
-// // // }
-
-// // // const func3 = (str) => str;
-
-// // // console.log(func2("func2"))
-
-// // // console.log(func3("func3"))
-
-// // const myProfile = {
-// //   name: "tomo",
-// //   age: 28,
-// // }
-
-// // const message3 = `my name is ${myProfile.name}. my age is ${myProfile.age}.`
-// // console.log(message3)
-
-// // const { name, age } = myProfile;
-// // const message4 = `my name is ${name}. my age is ${age}.`
-// // console.log(message4)
-
-// // const myProfile = ["tomo", 28]
-// // const message3 = `my name is ${myProfile[0]}, my age is ${myProfile[1]}`
-// // console.log(message3)
-
-// // const [name, age] = myProfile
-// // const message4 = `my name is ${name}, my age is ${age}`
-// // console.log(message4)
-
-// // const arr1 = [1,2]
-// // console.log(arr1)
-// // console.log(...arr1)
-
-// // const sumFunc = (num1, num2) => console.log(num1 + num2)
-
-// // // sumFunc(arr1[0], arr1[1])
-// // sumFunc(...arr1)
-
-// const arr2 = [1,2,3,4,5]
-// const [num1,num2,...arr3] = arr2
-// console.log(num1) // 1
-// console.log(num2) // 2
-// console.log(arr3) // 3.4.5
-
-//
-
-const nameArr = ["A", "B", "C"];
-const nameArr2 = nameArr.map((name) => {
-  return name;
-});
-console.log(nameArr2);
-
-const newNameArr = nameArr.map((name) => {
-  if (name === "A") {
-    return name;
-  } else {
-    return `Mr. ${name}`;
-  }
-});
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
